8b9deb92b135fed9763aa14eddfc1fee74e9a389
Dhruva Gole <goledhruva@gmail.com>
2021-10-10 21:53:19 +0530

    fork.c: Remove warnings and improve demo
    Signed-off-by: Dhruva Gole <goledhruva@gmail.com>

--- interview-prep/c-programs/fork.c
+++ interview-prep/c-programs/fork.c
@@ -22,18 +22,18 @@
 #include	<stdlib.h>
 
 pid_t fork(void);
-printChild(float* a , float* b )
+void printChild(float* a , float* b )
 {
 	register int i = 0;
 	*b = *a + *b;
-	printf("{PID:%d}Inside func, a+b = %f\n",getpid(), *b);
+	printf("{PID:%d} Inside func, a+b = %f\n",getpid(), *b);
 	while(i < 5)
 	{
-	printf("{PID:%d}{count = %d}Inside func, b+b = %f\n",getpid(), i, (*b)+=*b);
-	sleep(1);
-	i++;
+		printf("{PID:%d | count:%d} Inside func, b+b = %f\n",getpid(), i, (*b)+=*b);
+		sleep(1);
+		i++;
 	}
-	printf("address {b = 0x%x}\n", b);
+	return;
 }
 
 /*
@@ -45,23 +45,44 @@ printChild(float* a , float* b )
 main ( int argc, char *argv[] )
 {
 	float A = 15.333, B = 22.777;
+	register unsigned int count = 0;
 	if (fork() == 0) {
 		printChild(&A,&B);
 	}
 	else {
-		printf("{PID:%d}Hello, I am main()!\n", getpid());
-		sleep(5);
+		while ( count < 15 )
+		{
+			++count;
+			printf("{PID:%d | count:%d} Hello, I am main()!\n", getpid(), count);
+			sleep(1.05);
+		}
 	}
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
 
 /* OUTPUT -----------------------------------------------------------------------------
-{PID:56812}Hello, I am main()!
-{PID:56813}Inside func, a+b = 38.110001
-{PID:56813}{count = 0}Inside func, b+b = 76.220001
-{PID:56813}{count = 1}Inside func, b+b = 152.440002
-{PID:56813}{count = 2}Inside func, b+b = 304.880005
-{PID:56813}{count = 3}Inside func, b+b = 609.760010
-{PID:56813}{count = 4}Inside func, b+b = 1219.520020
-address {b = 0x36541134}
+   > $ ./myfork
+	   {PID:67985}Hello, I am main()!
+	   {PID:67986}Inside func, a+b = 38.110001
+	   {PID:67986}{count = 0}Inside func, b+b = 76.220001
+	   {PID:67985}Hello, I am main()!
+	   {PID:67986}{count = 1}Inside func, b+b = 152.440002
+	   {PID:67985}Hello, I am main()!
+	   {PID:67986}{count = 2}Inside func, b+b = 304.880005
+	   {PID:67985}Hello, I am main()!
+	   {PID:67986}{count = 3}Inside func, b+b = 609.760010
+	   {PID:67985}Hello, I am main()!
+	   {PID:67986}{count = 4}Inside func, b+b = 1219.520020
+	   {PID:67985}Hello, I am main()!
+	   address {b = 0xf72511b4}
+	   {PID:67985}Hello, I am main()!
+	   {PID:67985}Hello, I am main()!
+	   {PID:67985}Hello, I am main()!
+	   {PID:67985}Hello, I am main()!
+	   {PID:67985}Hello, I am main()!
+	   {PID:67985}Hello, I am main()!
+	   {PID:67985}Hello, I am main()!
+	   {PID:67985}Hello, I am main()!
+	   {PID:67985}Hello, I am main()!
+
 -----------------------------------------------------------------------------*/
